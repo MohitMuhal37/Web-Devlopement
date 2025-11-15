947c105632be10de2691dc60832372e11153cb41
Mohit Muhal <mohitmuhal15122003@gmail.com>
2025-11-15 20:00:14 +0530

    new lines of code are added

--- assets/scripts/calc.js
+++ assets/scripts/calc.js
@@ -2,4 +2,12 @@ let currentResult = 0;
 
 currentResult = currentResult + 10 * 5;
 
+function addNumbers(num1, num2){
+        return num1 + num2;
+}
+
+alert("The result is = "+addNumbers(4, 6))
+
 outputResult(currentResult, '');
+
+
